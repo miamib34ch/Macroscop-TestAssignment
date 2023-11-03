92d3ebaba73d9b0b334e79f33a68fd722a218ed6
Bogdan <bogdan.polygalov@gmail.com>
2023-11-04 00:21:43 +0500

    добавил метод для загрузки картинки

--- Macroscop-TestAssignment/CameraPreviewsViewModel.swift
+++ Macroscop-TestAssignment/CameraPreviewsViewModel.swift
@@ -11,19 +11,34 @@ import Alamofire
 typealias Binding<T> = (T) -> Void
 
 final class CameraPreviewsViewModel {
-    var onResponseReceive: Binding<[CameraPreview]?>?
+    var onServerConfigResponseReceive: Binding<[CameraPreview]?>?
 
     func fetchServerConfig() {
         AF.request(Constants.serverRoot + Constants.serverConfigEndpoint, parameters: ["login": "root", "responsetype": "json"]).validate().responseDecodable(of: ServerConfig.self) { [weak self] response in
             switch response.result {
             case .success(let serverConfig):
                 let cameraPreviews = self?.createCameraPreviews(serverConfig: serverConfig)
-                self?.onResponseReceive?(cameraPreviews)
+                self?.onServerConfigResponseReceive?(cameraPreviews)
             case .failure(let error):
-                print("CamerasPreviewViewModel_fetchData_error: \(error.localizedDescription)")
-                self?.onResponseReceive?(nil)
+                print("CamerasPreviewViewModel_fetchServerConfig_error: \(error.localizedDescription)")
+                self?.onServerConfigResponseReceive?(nil)
             }
         }
+    }
+
+    func fetchPreviewImage(cameraId: String) async throws -> Data {
+        let data = try await withCheckedThrowingContinuation { continuation in
+            AF.request(Constants.serverRoot + Constants.serverMobileEndpoint, parameters: ["channelid": "\(cameraId)", "oneframeonly": "true", "login": "root", "withcontenttype": "true"]).validate().responseData { response in
+                switch response.result {
+                case .success(let data):
+                    continuation.resume(returning: data)
+                case .failure(let error):
+                    print("CamerasPreviewViewModel_fetchPreviewImage_error: \(error.localizedDescription)")
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        return data
     }
 
     private func createCameraPreviews(serverConfig: ServerConfig) -> [CameraPreview] {
