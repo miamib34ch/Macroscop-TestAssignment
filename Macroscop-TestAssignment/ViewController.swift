4cba9922a83857e04da5997ee8b7fd97fd85c089
Bogdan <bogdan.polygalov@gmail.com>
2023-11-03 18:17:33 +0500

    Initial Commit

---
+++ Macroscop-TestAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Macroscop-TestAssignment
+//
+//  Created by Богдан Полыгалов on 03.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
