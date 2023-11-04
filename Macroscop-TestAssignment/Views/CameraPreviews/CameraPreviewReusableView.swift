20dcba1b68c2c334f372a5b2eb05e282798ca217
Bogdan <bogdan.polygalov@gmail.com>
2023-11-04 18:37:17 +0500

    Добавил header для секций коллекции

--- Macroscop-TestAssignment/Views/CameraPreviews/CameraPreviewReusableView.swift
+++ Macroscop-TestAssignment/Views/CameraPreviews/CameraPreviewReusableView.swift
@@ -8,4 +8,39 @@
 import UIKit
 
 final class CameraPreviewReusableView: UICollectionReusableView, ReusableHeaderIdentifying {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = UIFont.headline3
+        return label
+    }()
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = ""
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setTitle(text: String) {
+        titleLabel.text = text
+    }
+
+    private func setView() {
+        backgroundColor = .clear
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
 }
